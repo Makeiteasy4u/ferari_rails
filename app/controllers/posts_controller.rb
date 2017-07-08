0f34b0401e4a83e77eb3fc68bd9c0edd217a6e51
Makeiteasy4u <tamer2555@gmail.com>
2017-07-07 18:52:23 -0700

    Intial release: 1- generate DB migrate, 2- update the migrated file, 3- run db:migrate, 4-add modele, 5- add resource to routes file, 6- Create a new controller, 7- add actions & add templates in view into created folder, 9- add messages, 10- create partials(_form, _messages), 11- add link_to to inportat links, 12- add home & About page and add link_to all pots listenner , Finaly to git

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        flash[:notice] = 'Post are succefully created'
+        redirect_to post_path(@post)
+      else
+        render 'new'
+      end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+      if @post.update(post_params)
+        flash[:notice] = 'Post are succefully updated'
+        redirect_to post_path(@post)
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = 'post succefully deleted'
+    redirect_to posts_path(@post)
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+
+end
